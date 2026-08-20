7c251142bff87bce261bd9fe1ba87569a5f9ded1
leoAraya <leoar1356@gmail.com>
2026-08-20 19:28:00 -0300

    Agrego clase 1 de JavaScript

---
+++ Semestre 2/JavaScript/Clase 1 Ciclos/Ciclos.js
@@ -0,0 +1,39 @@
+// While
+let contador = 0;
+while(contador < 3) {
+    console.log(contador);
+    contador++;
+}
+console.log('Fin del ciclo While');
+
+// Do While
+let conteo = 0;
+do {
+    console.log(conteo);
+    conteo++;
+} while (conteo < 3);
+console.log('Fin del ciclo Do While')
+
+// For
+for(let contando = 0; contando < 3; contando++){
+    console.log(contando);
+}
+console.log('Fin del ciclo For');
+
+// Palabra reservada: Break
+for(let contando = 0; contando <= 10; contando++){
+    if(contando % 2 == 0){
+        console.log(contando);
+        break;
+    }
+}
+console.log('Termina el ciclo al encontrar el primer numero par');
+
+// Palabra reservada: Continue
+for(let contando = 0; contando <= 10; contando++){
+    if(contando % 2 !== 0){
+        continue; // Va a la siguiente iteracion
+    }
+    console.log(contando);
+}
+console.log('Termina el ciclo');
---
+++ Semestre 2/JavaScript/Clase 1 Ciclos/Labels.js
@@ -0,0 +1,9 @@
+// Etiquetas Labels
+inicio:
+for(let contando = 0; contando <= 10; contando++){
+    if(contando % 2 !== 0){
+        break inicio; // Va a la siguiente iteracion
+    }
+    console.log(contando);
+}
+console.log('Termina el ciclo');
